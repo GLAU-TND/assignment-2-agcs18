315e29c59803fdbf507b48b4bfb3dc5e0378f383
Aishwarya Gupta <aishwarya.gupta_da18@gla.ac.in>
2020-03-28 18:13:29 +0530

    Student Class Initialisation

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,22 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    public int roll;
+    public int a_count;
+    public int backlog;
+
+    public Student(int roll, int a_count, int backlog) {
+        this.roll = roll;
+        this.a_count = a_count;
+        this.backlog = backlog;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "roll=" + roll +
+                ", a_count=" + a_count +
+                ", backlog=" + backlog +
+                '}';
+    }
 }

8e87e1d0cd32ab06b2dcf50bf3d05bb957932058
Aishwarya Gupta <aishwarya.gupta_da18@gla.ac.in>
2020-03-28 18:29:37 +0530

    Main Class

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,20 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+import problem5.circularqueue.MyCircularQueue;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(1, 1, 0);
+        obj.enqueue(2, 1, 1);
+        obj.enqueue(3, 2, 4);
+        obj.enqueue(4, 2, 1);
+        obj.enqueue(5, 3, 2);
+        obj.enqueue(6, 3, 0);
+        obj.removeBackLog();
+        obj.display();
+
+    }
 }

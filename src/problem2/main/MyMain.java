e26b69c7f164d7415df7d6b59ef18c9894f04006
Aishwarya Gupta <aishwarya.gupta_da18@gla.ac.in>
2020-03-28 17:37:24 +0530

    Pre and Post order

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -6,6 +6,18 @@
  */
 package problem2.main;
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insertRec(2);
+        obj.insertRec(8);
+        obj.insertRec(1);
+        obj.insertRec(5);
+        obj.insertRec(55);
+        obj.insertRec(30);
+        obj.insertRec(20);
+        obj.previouspost();
+    }
 }

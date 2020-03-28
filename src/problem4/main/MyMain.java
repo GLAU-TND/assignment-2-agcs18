ec9e90e8e92aa5fe43a91c68093962d8f82ade84
Aishwarya Gupta <aishwarya.gupta_da18@gla.ac.in>
2020-03-28 18:09:13 +0530

    Main Class Initiation

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -12,17 +12,17 @@ import problem4.myqueue.MyQueue;
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree obj = new MyBinarySearchTree();
-        obj.insertRec(10);
-        obj.insertRec(3);
+        obj.insertRec(20);
         obj.insertRec(30);
-        obj.insertRec(37);
-        obj.insertRec(13);
-        obj.insertRec(1);
+        obj.insertRec(40);
+        obj.insertRec(57);
+        obj.insertRec(63);
         obj.insertRec(7);
+        obj.insertRec(80);
 
         MyQueue my = new MyQueue();
         my.preSuccessor(obj.root);
         my.getPreSucc();
 
-
+    }
     }

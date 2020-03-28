d1b8d2ee1b9a8bc3dc17a403a5469513b24f9856
Aishwarya Gupta <aishwarya.gupta_da18@gla.ac.in>
2020-03-28 18:02:55 +0530

    Enqueue Operation

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,34 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        data = data;
+        next = null;
+    }
+}
+
 public class MyQueue {
+    Node front;
+    Node rear;
+
+    public MyQueue() {
+        front = null;
+        rear = null;
+    }
+
+    public void enqueue(int data) {
+        Node node = new Node(data);
+        node.data = data;
+        if (front == null) {
+            front = node;
+            rear = node;
+        } else {
+            rear.next = node;
+            rear = node;
+        }
+    }
 }

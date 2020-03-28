dec971b9585b853a39e9ecec43e3a892a5cd19dc
Aishwarya Gupta <aishwarya.gupta_da18@gla.ac.in>
2020-03-28 17:49:07 +0530

    Pre and Post order

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,33 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    Node head;
+
+    public MyPriorityQueue() {
+        head = null;
+    }
+
+    public void insert(String name, int roll) {
+        Node ns = new Node(name, roll);
+        ns.name = name;
+        ns.roll = roll;
+        Node temp = head;
+        if (head == null) {
+            head = ns;
+        } else {
+            if (head.roll > roll) {
+                ns.next = head;
+                head = temp;
+            } else {
+                while (temp.next != null && temp.next.roll < roll) {
+                    temp = temp.next;
+                }
+                ns.next = temp.next;
+                temp.next = ns;
+            }
+        }
+    }
 }
